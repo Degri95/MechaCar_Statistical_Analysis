7e2c1e34b4f874564134672491909a16ea8cc79f
David Egri <david.egri95@gmail.com>
2022-09-25 16:43:56 -0700

    Updating Script

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,16 +1,31 @@
 library(dplyr)
+
+#Importing MPG Data
 MechaData <- read.csv("MechaCar_mpg.csv")
+
+#Linear Regression
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaData)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaData))
 
-
+# Importing suspension data
 SuspensionData <- read.csv("Suspension_coil.csv")
+
+# Total summary
 total_summary <- SuspensionData %>% summarize(Mean=mean(SuspensionData$PSI),
                                               Median=median(SuspensionData$PSI),
                                               Variance=var(SuspensionData$PSI),
                                               SD=sd(SuspensionData$PSI))
 
+# Lot summary
 lot_summary <- SuspensionData %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
                                                                             Median=median(PSI),
                                                                             Variance=var(PSI),
                                                                             SD=sd(PSI))
+# T Test on all manufacturers
+t.test(SuspensionData$PSI, mu=1500)
+# T Test on lot1
+t.test(subset(SuspensionData, Manufacturing_Lot == "Lot1")$PSI, mu=1500)
+# T Test on lot2
+t.test(subset(SuspensionData, Manufacturing_Lot == "Lot2")$PSI, mu=1500)
+# T Test on lot3
+t.test(subset(SuspensionData, Manufacturing_Lot == "Lot3")$PSI, mu=1500)
